723284407709c868e5f440d5f35b7a54cec5b7b1
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-13 19:24:22 +0300

    first commit

--- AngryBirdClone/GameViewController.swift
+++ AngryBirdClone/GameViewController.swift
@@ -26,9 +26,9 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+           
+            view.showsPhysics = true
+           
         }
     }
 
